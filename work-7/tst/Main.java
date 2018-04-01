60ea71fc5738921bde61b0ba13027ce5f22a0bdf
LvLs9 <lunev432@gmail.com>
2018-04-02 01:54:14 +0300

    Create Main() and settled out directory

--- work-7/tst/Main.java
+++ work-7/tst/Main.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String[] args) {
         ExpressionParser parser = new ExpressionParser();
         try {
-            String testStr = "-2147483648";
+            String testStr = "pow10 x * y";
 
             TripleExpression expression = parser.parse(testStr);
-            System.err.println(expression.evaluate(0, 0, 0));
+            System.err.println(expression.evaluate(10, 0, 0));
         } catch (Exception e) {
             e.printStackTrace();
         }
