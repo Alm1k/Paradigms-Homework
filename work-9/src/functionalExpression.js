b1197465931462d0fd60afd6d81a67833e3b0ec9
LvLs9 <lunev432@gmail.com>
2018-04-23 15:42:04 +0300

    work-9 easy modification

--- work-9/src/functionalExpression.js
+++ work-9/src/functionalExpression.js
@@ -1,24 +1,57 @@
 'use strict';
 
-// unary
 var cnst = function (value) {
-    return function (x) {
+    return function (x, y, z) {
         return value;
     }
-}
+};
 
 var variable = function (name) {
-    return function (x) {
-        return x;
+    return function (x, y, z) {
+        switch (name) {
+            case 'x':
+                return x;
+            case 'y':
+                return y;
+            case 'z':
+                return z;
+        }
     }
-}
+};
+
+// unary
+var unary = function (apply) {
+    return function (expression) {
+        return function (x, y, z) {
+            return apply(expression(x, y, z));
+        }
+    }
+};
+
+var negate = unary(
+    function (expression) {
+        return -expression;
+    }
+);
+
+var cube = unary(
+    function (expression) {
+        return Math.pow(expression, 3);
+    }
+);
+
+var cuberoot = unary(
+    function (expression) {
+        return Math.pow(expression, 1/3);
+    }
+);
 
 
 // binary
 var binary = function (apply) {
     return function (left, right) {
-        return function (x) {
-            return apply(left(x), right(x));
+        return function (x, y, z) {
+            return apply(left(x, y, z), right(x, y, z));
         };
     }
 };
